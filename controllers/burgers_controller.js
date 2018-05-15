3376bf799c3132f51a0b6d20bce55b26301ffeb2
hkichen <hkichen@gmail.com>
2018-05-15 16:21:55 -0400

    setting up routes in burger_controllers

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,3 +2,32 @@
 //Express
 //`burger.js`
 //Create the `router` for the app, and export the `router` at the end of your file.
+
+var burger = require("../models/burger.js");
+var express = require("express");
+var router = express.Router();
+
+//set up routes
+router.get("/", function(req, res) {
+  burger.selectAll(function(data) {
+    var hbsObj = {
+      burgers: data
+    }
+    console.log(hbsObj);
+    res.render("index", hbsObj);
+  });
+});
+
+router.post("/api/burgers", function(req, res) {
+  burger.insertOne([
+    "burger_name", "Turkey Burger"
+  ], [
+    req.body.name, req.body.turkeyBurger
+  ], function(result) {
+    res.json({
+      id: result.insertId 
+    });
+  });
+});
+
+
