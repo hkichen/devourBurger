45eecdeca1db1895d46bdacda5aca2cb4a694700
hkichen <hkichen@gmail.com>
2018-05-15 12:18:49 -0400

    save changes to connection page --made it a module export

--- config/connection.js
+++ config/connection.js
@@ -1,2 +1,22 @@
 //Inside the `connection.js` file, setup the code to connect Node to MySQL.
 //Export the connection.
+
+var mysql = require("mysql");
+
+var connection = mysql.createConnection({
+  port: 3306,
+  host: 'localhost',
+  user: 'root',
+  password: 'pass',
+  database: 'burgers_db'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.log("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId)
+ });
+
+ module.exports = connection;
