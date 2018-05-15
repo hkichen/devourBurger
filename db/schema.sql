f6b42dc34e7d02831732f3478e73e24b7152ea7e
hkichen <hkichen@gmail.com>
2018-05-15 12:18:05 -0400

    set up connection.js page

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+drop database if exists burgers_db;
+create database burgers_db;
+
+use burgers_db;
+
+drop table if exists burgers;
+create table burgers (
+  id int auto_increment not null,
+  burger_name varchar(150) not null,
+  devoured boolean default 0 not null,
+  primary key (id)
+);
+
---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+insert into burgers (burger_name, devoured)
+values ("Double Cheese Burger", 0);
+
+insert into burgers (burger_name, devoured)
+values ("Goat Cheese and fig relish Burger", 0);
+
+insert into burgers (burger_name, devoured)
+values ("Baconator Burger", 0);
+
+insert into burgers (burger_name, devoured)
+values ("Bison Burger", 0);
+
+insert into burgers (burger_name, devoured)
+values ("Smoked BBQ Burger", 0);
